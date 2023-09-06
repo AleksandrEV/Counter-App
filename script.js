57eb68607efbe05cfde38602c05c860c75bece22
Aleksandr <aleksandr.elovskih04@gmail.com>
2023-09-06 21:31:58 +0500

    Добавлена кнопка обнуления счетчика, ограничено вычитание ниже 0

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let subsrtuct = document.getElementById('substract');
 let add = document.getElementById('add');
+let output = document.getElementById('result');
 
+subsrtuct.addEventListener('click', function () {
+   let result = Number(output.textContent) - 1;
+   if (result >= 0) {
+      output.textContent = result;
+   }
+})
 
+add.addEventListener('click', function () {
+   let result = Number(output.textContent) + 1;
+   output.textContent = result;
+})
 
-//console.log(subsrtuct, add);
+document.getElementById('null').addEventListener('click', function () {
+   output.innerHTML = 0;
+})
